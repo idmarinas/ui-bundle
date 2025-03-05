9803868d419137c8ed97e0d6b44014345a3b2c1f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-05 22:40:18 +0100

    feat(services): register Alert component in service configuration
    - Added the Alert component to the service configuration in
      `config/services.php`. This allows the component to be
      tagged as a Twig component for use in templates.

--- config/services.php
+++ config/services.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 16/12/2024, 19:06
+ * Last modified by "IDMarinas" on 05/03/2025, 20:55
  *
  * @project IDMarinas Ui Bundle
  * @see     https://github.com/idmarinas/ui-bundle
  *
- * @file    service.php
+ * @file    services.php
  * @date    05/03/2025
  * @time    15:51
  *
@@ -19,10 +19,13 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Idm\Bundle\Ui\Twig\Component\Element\Alert;
+
 return function (ContainerConfigurator $container) {
 	// @formatter:off
 	$container->services()
-
+		->set('.idm_ui.twig_component.element.alert', Alert::class)
+			->tag('twig.component')
 	;
 	// @formatter::on
 };
