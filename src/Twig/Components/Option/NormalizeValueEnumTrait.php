04161d569bda8408d6e79ccdf6c2d1c824b7bc61
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-05-06 14:03:24 +0200

    Update namespace for NormalizeValueEnumTrait
    Renamed the namespace from "Component" to "Components" to align with the proper directory structure. This change ensures consistency and avoids potential autoloading issues.

--- src/Twig/Components/Option/NormalizeValueEnumTrait.php
+++ src/Twig/Components/Option/NormalizeValueEnumTrait.php
@@ -2,22 +2,22 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 06/03/2025, 23:55
+ * Last modified by "IDMarinas" on 06/05/2025, 14:02
  *
  * @project IDMarinas Ui Bundle
- * @see     https://github.com/idmarinas/ui-bundle
+ * @see https://github.com/idmarinas/ui-bundle
  *
- * @file    NormalizeValueEnumTrait.php
- * @date    06/03/2025
- * @time    15:34
+ * @file NormalizeValueEnumTrait.php
+ * @date 06/03/2025
+ * @time 15:34
  *
- * @author  Iván Diaz Marinas (IDMarinas)
+ * @author Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
-namespace Idm\Bundle\Ui\Twig\Component\Option;
+namespace Idm\Bundle\Ui\Twig\Components\Option;
 
 trait NormalizeValueEnumTrait
 {
