08ff5d912b87f64f0ee3eff132394696986e856f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-05-06 14:03:54 +0200

    Rename namespace to fix typo in file structure alignment.
    The namespace was updated from "Component" to "Components" to match the file structure. This resolves potential issues with autoloading and ensures consistency across the codebase.

--- src/Twig/Components/Container/NotificationMessages.php
+++ src/Twig/Components/Container/NotificationMessages.php
@@ -2,22 +2,22 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 06/03/2025, 21:13
+ * Last modified by "IDMarinas" on 06/05/2025, 14:02
  *
  * @project IDMarinas Ui Bundle
- * @see     https://github.com/idmarinas/ui-bundle
+ * @see https://github.com/idmarinas/ui-bundle
  *
- * @file    NotificationMessages.php
- * @date    06/03/2025
- * @time    19:20
+ * @file NotificationMessages.php
+ * @date 06/03/2025
+ * @time 19:20
  *
- * @author  Iván Diaz Marinas (IDMarinas)
+ * @author Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
-namespace Idm\Bundle\Ui\Twig\Component\Container;
+namespace Idm\Bundle\Ui\Twig\Components\Container;
 
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\TwigComponent\Attribute\PreMount;
