f77cdc75c398970535a670539f200df6f0237f04
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-05-06 14:04:35 +0200

    Update namespaces in services configuration file
    Adjusted the namespaces for UI Twig components to reflect the correct directory structure. This change ensures proper loading and avoids potential errors caused by outdated or incorrect namespace paths.

--- config/services.php
+++ config/services.php
@@ -2,27 +2,27 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 06/03/2025, 19:33
+ * Last modified by "IDMarinas" on 06/05/2025, 14:02
  *
  * @project IDMarinas Ui Bundle
- * @see     https://github.com/idmarinas/ui-bundle
+ * @see https://github.com/idmarinas/ui-bundle
  *
- * @file    services.php
- * @date    05/03/2025
- * @time    15:51
+ * @file services.php
+ * @date 05/03/2025
+ * @time 15:51
  *
- * @author  Iván Diaz Marinas (IDMarinas)
+ * @author Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Idm\Bundle\Ui\Twig\Component\Container\FlashMessages;
-use Idm\Bundle\Ui\Twig\Component\Container\NotificationMessages;
-use Idm\Bundle\Ui\Twig\Component\Element\Alert;
-use Idm\Bundle\Ui\Twig\Component\Element\Notification;
+use Idm\Bundle\Ui\Twig\Components\Container\FlashMessages;
+use Idm\Bundle\Ui\Twig\Components\Container\NotificationMessages;
+use Idm\Bundle\Ui\Twig\Components\Element\Alert;
+use Idm\Bundle\Ui\Twig\Components\Element\Notification;
 
 return function (ContainerConfigurator $container) {
 	// @formatter:off
