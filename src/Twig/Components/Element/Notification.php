be303b6f5eae2a1803c9de44c61f2c7a101e3558
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-05-08 19:38:57 +0200

    refactor: simplify normalizers and remove unused notification class methods

--- src/Twig/Components/Element/Notification.php
+++ src/Twig/Components/Element/Notification.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 06/05/2025, 14:02
+ * Last modified by "IDMarinas" on 08/05/2025, 19:36
  *
  * @project IDMarinas Ui Bundle
  * @see https://github.com/idmarinas/ui-bundle
@@ -68,55 +68,16 @@ final class Notification
 			->setAllowedTypes('id', ['int', 'string', 'null'])
 			->setAllowedValues('type', fn(string|MessageVariantEnum $value) => MessageVariantEnum::isValidValue($value))
 			->setAllowedValues('duration', Validation::createIsValidCallable(new DivisibleBy(5000)))
-			->setNormalizer('closable', function (Options $opts, int $value) {
-				// Force to be closable if duration is 5 times the default duration
-				if ($opts['duration'] / 5000 >= 5) {
-					$value = true;
-				}
-
-				return $value;
-			})
+			->setNormalizer('closable', fn (Options $opts, bool $value) => ($opts['duration'] / 5000 >= 5))
 			->setNormalizer(
 				'type',
 				fn(Options $opts, string|MessageVariantEnum $type) => MessageVariantEnum::normalizeValue($type)
 			)
-			->setNormalizer(
-				'title',
-				fn(Options $opts, string|TranslatableInterface $value) => $this->translateOrSanitize($value)
-			)
-			->setNormalizer(
-				'message',
-				fn(Options $opts, string|TranslatableInterface $value) => $this->translateOrSanitize($value)
-			)
+			->setNormalizer('title', fn(Options $opts, string|TranslatableInterface $value) => $this->translateOrSanitize($value))
+			->setNormalizer('message', fn(Options $opts, string|TranslatableInterface $value) => $this->translateOrSanitize($value))
 		;
 
 		return $resolver->resolve($data) + $data;
-	}
-
-	#[ExposeInTemplate('notification_class')]
-	public function getNotificationClass (): string
-	{
-		return match ($this->type) {
-			MessageVariantEnum::Error,
-			MessageVariantEnum::Danger  => 'bg-red-400 border-red-700 text-red-50',
-			MessageVariantEnum::Success => 'bg-green-400 border-green-700 text-green-50',
-			MessageVariantEnum::Warning => 'bg-yellow-400 border-yellow-700 text-yellow-50',
-			MessageVariantEnum::Info    => 'bg-blue-400 border-blue-700 text-blue-50',
-			MessageVariantEnum::Notice  => 'bg-gray-400 border-gray-700 text-gray-50',
-		};
-	}
-
-	#[ExposeInTemplate('icon_class_cnt')]
-	public function getIconClassCnt (): string
-	{
-		return match ($this->type) {
-			MessageVariantEnum::Error,
-			MessageVariantEnum::Danger  => 'bg-red-100 border-red-500 text-red-400',
-			MessageVariantEnum::Success => 'bg-green-100 border-green-500 text-green-400',
-			MessageVariantEnum::Warning => 'bg-yellow-100 border-yellow-500 text-yellow-400',
-			MessageVariantEnum::Info    => 'bg-blue-100 border-blue-500 text-blue-400',
-			MessageVariantEnum::Notice  => 'bg-gray-100 border-gray-500 text-gray-400',
-		};
 	}
 
 	#[ExposeInTemplate('icon_name')]
