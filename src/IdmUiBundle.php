05329f4dc1059a1265b0d6bf85c96d60ec4c43f0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-05-09 18:26:26 +0200

    refactor: update name_prefix to use IdmUi instead of UI in Twig components configuration

--- src/IdmUiBundle.php
+++ src/IdmUiBundle.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 09/05/2025, 16:59
+ * Last modified by "IDMarinas" on 09/05/2025, 18:22
  *
  * @project IDMarinas Ui Bundle
  * @see     https://github.com/idmarinas/ui-bundle
@@ -37,7 +37,7 @@ final class IdmUiBundle extends AbstractBundle
 			'defaults' => [
 				'Idm\\Bundle\\Ui\\Twig\\Components\\' => [
 					'template_directory' => '@IdmUi/components',
-					'name_prefix'        => 'UI',
+					'name_prefix'        => 'IdmUi',
 				],
 			],
 		]);
