6302ecd14c5cfc17e764bff28849ca83073207c6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-08 13:29:01 +0100

    refactor(tests): move doctrine.php configuration to tests directory for improved test environment isolation

--- tests/app/config/packages/doctrine.php
+++ tests/app/config/packages/doctrine.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 22/01/2025, 13:26
+ * Last modified by "IDMarinas" on 08/01/2026, 13:28
  *
  * @project IDMarinas Ui Bundle
  * @see     https://github.com/idmarinas/ui-bundle
@@ -21,12 +21,13 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Idm\Bundle\Ui\IdmUiBundle;
 use ReflectionClass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Filesystem\Filesystem;
 use function Symfony\Component\String\u;
 
-return static function (ContainerConfigurator $container) {
-	$getDatabaseCache = function (): string {
-		$dir = dirname(__DIR__, 3) . '/var/cache/database';
+return static function (ContainerConfigurator $container, ContainerBuilder $builder) {
+	$getDatabaseCache = function (string $projectDir, string $env): string {
+		$dir = $projectDir . '/var/cache/database';
 
 		$filesystem = new Filesystem();
 
@@ -34,26 +35,23 @@ return static function (ContainerConfigurator $container) {
 			$filesystem->mkdir($dir);
 		}
 
-		return $dir;
-	};
+		$dbName = (new ReflectionClass(IdmUiBundle::class))->getShortName();
+		$dbName = u($dbName)->snake()->toString();
 
-	$dbName = (new ReflectionClass(IdmUiBundle::class))->getShortName();
-	$dbName = u($dbName)->snake()->toString();
+		return sprintf('sqlite:///%s/%s_%s.sqlite', $dir, $dbName, $env);
+	};
 
 	$container->extension('doctrine', [
 		'dbal' => [
-			'driver'         => 'pdo_sqlite',
-			'url'            => sprintf('sqlite:///%s/%s_%s.sqlite', $getDatabaseCache(), $dbName, $container->env()),
-			'use_savepoints' => true,
+			'driver' => 'pdo_sqlite',
+			'url'    => $getDatabaseCache($builder->getParameter('kernel.project_dir'), $container->env()),
 		],
 		'orm'  => [
-			'enable_lazy_ghost_objects'   => true,
-			'auto_generate_proxy_classes' => true,
-			'auto_mapping'                => false,
-			'controller_resolver'         => [
+			'auto_mapping'        => false,
+			'controller_resolver' => [
 				'auto_mapping' => false,
 			],
-			'mappings'                    => [
+			'mappings'            => [
 				'Tests' => [
 					'is_bundle' => false,
 					'mapping'   => true,
@@ -61,10 +59,10 @@ return static function (ContainerConfigurator $container) {
 					'dir'       => dirname(__DIR__, 2) . '/src/Entity',
 					'prefix'    => 'App\Entity',
 				],
-				//'resolve_target_entities' => [
-				//	AbstractUser::class => User::class,
-				//],
 			],
+			//'resolve_target_entities' => [
+			//	AbstractUser::class => User::class,
+			//],
 		],
 	]);
 };
