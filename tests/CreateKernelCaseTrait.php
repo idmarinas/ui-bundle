066f8bf4602d464367b3440bd86ef3f6be83a1fd
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-08 13:19:21 +0100

    feat(tests): add CreateKernelCaseTrait for customizable kernel handling in test cases

---
+++ tests/CreateKernelCaseTrait.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 04/01/2026, 18:38
+ *
+ * @project IDMarinas Ui Bundle
+ * @see     https://github.com/idmarinas/ui-bundle
+ *
+ * @file    CreateKernelCaseTrait.php
+ * @date    13/03/2025
+ * @time    19:40
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace Idm\Bundle\Template\Tests;
+
+use App\Kernel;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+trait CreateKernelCaseTrait
+{
+	protected static function createKernel (array $options = []): KernelInterface
+	{
+		/** @var Kernel $kernel */
+		$kernel = parent::createKernel($options);
+		$kernel->handleOptions($options);
+
+		return $kernel;
+	}
+}
